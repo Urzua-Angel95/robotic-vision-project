efd0b601678f20d60f734f68fcaf417b3283f0e3
Ángel Urzúa <urzua@universidad.net>
2026-05-10 09:56:47 -0600

    setup

--- Conteo_Vision_Robotica/setup.py
+++ Conteo_Vision_Robotica/setup.py
@@ -1,2 +1,28 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
+setup(
+   
+    name="vision_lib",
+   
+    version="0.1.0",
+    
+ 
+    description="Libreria para segmentacion, conteo y analisis de objetos industriales.",
+    
+    
+    author="Equipo10_Rodriguez_Carreon_Urzua_Hernandez",
+    
+   
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    
+   
+    install_requires=[
+        "numpy",
+        "scipy",
+        "opencv-python",
+        "matplotlib"
+    ],
+    
+    python_requires=">=3.7",
+)
